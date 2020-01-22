2d356ed8f23bbc99f2026ddbf940ebc6a96a5c80
zen9easy <easyhun@zen9.co.kr>
2020-01-22 17:57:46 +0900

    dispatcher servlet 환경설정

---
+++ src/main/java/com/mad5/dragon/config/WebMvcConfig.java
@@ -0,0 +1,36 @@
+package com.mad5.dragon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.mad5.dragon" })
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+	/**
+	 * CSS / JavaScript / Image 등의 정적 리소스를 처리해주는 핸들러를 등록
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+	/**
+	 * JSP를 뷰로 사용하는 뷰 리졸버 등록
+	 */
+	@Bean
+    public ViewResolver jspViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
